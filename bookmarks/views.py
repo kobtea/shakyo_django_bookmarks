9d4ea4ba33066f5822c81c6fd8d1c5cadee93328
kobtea <kobtea@live.jp>
2014-03-24 00:27:04 +0900

    chapter4 user registration and management

---
+++ bookmarks/views.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, Http404
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from bookmarks.forms import RegistrationForm
+
+
+def main_page(request):
+    return render_to_response(
+        'main_page.html',
+        RequestContext(request)
+    )
+
+
+def user_page(request, username):
+    try:
+        user = User.objects.get(username=username)
+    except:
+        raise Http404('Requested user not found.')
+    bookmarks = user.bookmark_set.all()
+    variables = RequestContext(request, {
+        'username': username,
+        'bookmarks': bookmarks
+    })
+    return render_to_response('user_page.html', variables)
+
+
+def logout_page(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+                email=form.cleaned_data['email']
+            )
+            return HttpResponseRedirect('/register/success/')
+    else:
+        form = RegistrationForm()
+    variables = RequestContext(request, {'form': form})
+    return render_to_response('registration/register.html', variables)
--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,8 +1,14 @@
+import os.path
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from bookmarks.views import main_page, user_page
+from django.views.generic.base import TemplateView
+from bookmarks.views import main_page, user_page, logout_page, register_page
 admin.autodiscover()
+
+site_media = os.path.join(
+    os.path.dirname(__file__), 'site_media',
+)
 
 urlpatterns = patterns(
     '',
@@ -12,5 +18,12 @@ urlpatterns = patterns(
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', main_page),
-    url(r'user/(\w+)/$', user_page),
+    url(r'^user/(\w+)/$', user_page),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': site_media}),
+    url(r'^register/$', register_page),
+    url(r'^register/success/$', TemplateView.as_view(
+        template_name='registration/register_success.html')),
 )
