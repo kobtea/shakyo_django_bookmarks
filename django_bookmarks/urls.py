0390fa0d3528c6f8bc5805ee955cef26c9c99d4c
kobtea <kobtea@live.jp>
2014-03-23 20:24:01 +0900

    chapter3: building a social bookmarking application

--- django_bookmarks/urls.py
+++ django_bookmarks/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from bookmarks.views import main_page, user_page
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'django_bookmarks.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', main_page),
+    url(r'user/(\w+)/$', user_page),
 )
